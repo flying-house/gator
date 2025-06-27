1463fb8fe6f3db2ac13a1e86e568c1c9d92ef419
kylehouseholder <kylemhouseholder@gmail.com>
2025-06-27 09:42:46 -0500

    02:02 - initial commit, not working

---
+++ internal/config/config.go
@@ -0,0 +1,12 @@
+package config
+
+// Config gets... config?
+type Config struct {
+	dbURL           string
+	currentUserName string
+}
+
+// SetUser sets the user in the config file????
+func (c Config) SetUser() string {
+	return ""
+}
